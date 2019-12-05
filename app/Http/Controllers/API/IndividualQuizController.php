2c5dc33700beeb034746df4627c30b90a4f425e3
André Ascensão <andremiguelaa@gmail.com>
2019-12-05 23:18:46 +0000

    individual quizzes filters

--- app/Http/Controllers/API/IndividualQuizController.php
+++ app/Http/Controllers/API/IndividualQuizController.php
@@ -14,27 +14,47 @@ class IndividualQuizController extends BaseController
     public function list(Request $request)
     {
         $input = $request->all();
-        if (array_key_exists('id', $input)) {
-            if (!is_array($input['id'])) {
-                return $this->sendError('id_must_be_array', 400);
+        $query = IndividualQuiz::select('id', 'individual_quiz_type', 'date');
+
+        if (array_key_exists('individual_quiz_player_id', $input) && is_array($input['individual_quiz_player_id'])) {
+            $individualQuizPlayerQuizzes = IndividualQuizResult::whereIn('individual_quiz_player_id', $input['individual_quiz_player_id'])
+                ->get()
+                ->pluck('individual_quiz_id')
+                ->toArray();
+            $query->whereIn('id', $individualQuizPlayerQuizzes);
+        } elseif (array_key_exists('individual_quiz_player_id', $input) && !is_array($input['individual_quiz_player_id'])) {
+            return $this->sendError('filter_parameters_must_be_arrays', 400);
+        }
+
+        $validFilterKeys = ['id', 'individual_quiz_type', 'month'];
+        foreach ($input as $key => $value) {
+            if (in_array($key, $validFilterKeys) && is_array($value)) {
+                if ($key === 'month') {
+                    $tableKey = 'date';
+                    $ids = array_map(function ($item) {
+                        return $item . '-01';
+                    }, $value);
+                } else {
+                    $tableKey = $key;
+                    $ids = $value;
+                }
+                $query->whereIn($tableKey, $ids);
+            } elseif (in_array($key, $validFilterKeys) && !is_array($value)) {
+                return $this->sendError('filter_parameters_must_be_arrays', 400);
             }
-            $individualQuizzes = IndividualQuiz::whereIn('id', $input['id'])
-                    ->select('id', 'individual_quiz_type', 'date')
-                    ->get();
-            if ($individualQuizzes->count() != count($input['id'])) {
-                return $this->sendError('individual_quiz_not_found', 404);
-            }
-            foreach ($individualQuizzes as $individualQuiz) {
+        }
+
+        $individualQuizzes = $query->orderBy('date')->get();
+
+        foreach ($individualQuizzes as $individualQuiz) {
+            $individualQuiz->month = substr($individualQuiz->date, 0, -3);
+            unset($individualQuiz->date);
+            if (array_key_exists('results', $input)) {
                 $individualQuiz->results = $individualQuiz->results;
             }
-            return $this->sendResponse($individualQuizzes, 200);
         }
-        $response = array_map(function ($individualQuiz) {
-            $individualQuiz['month'] = substr($individualQuiz['date'], 0, -3);
-            unset($individualQuiz['date']);
-            return $individualQuiz;
-        }, IndividualQuiz::all('id', 'individual_quiz_type', 'date')->toArray());
-        return $this->sendResponse($response, 200);
+
+        return $this->sendResponse($individualQuizzes, 200);
     }
 
     public function create(Request $request)
