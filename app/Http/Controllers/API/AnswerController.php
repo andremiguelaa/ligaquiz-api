0fe40997117d5d0a2e017fa85f32f29ad5f7af96
André Ascensão <andremiguelaa@gmail.com>
2020-06-16 23:29:51 +0100

    answers endpoints improvements

--- app/Http/Controllers/API/AnswerController.php
+++ app/Http/Controllers/API/AnswerController.php
@@ -14,51 +14,62 @@ class AnswerController extends BaseController
 {
     public function get(Request $request)
     {
-        $input = $request::all();
-        $validator = Validator::make($input, [
-            'user_id' => 'exists:users,id',
-            'quiz' => 'required_without:special_quiz|exists:quizzes,id',
-            'special_quiz' => 'required_without:quiz|exists:special_quizzes,id',
-            'submitted' => 'boolean'
-        ]);
-        if ($validator->fails()) {
-            return $this->sendError('validation_error', $validator->errors(), 400);
+        if (
+            Auth::user()->hasPermission('quiz_play') ||
+            Auth::user()->hasPermission('specialquiz_play') ||
+            Auth::user()->hasPermission('answer_correct')
+        ) {
+            $input = $request::all();
+            $validator = Validator::make($input, [
+                'user_id' => 'exists:users,id',
+                'quiz' => 'required_without:special_quiz|exists:quizzes,id',
+                'special_quiz' => 'required_without:quiz|exists:special_quizzes,id',
+                'submitted' => 'boolean'
+            ]);
+            if ($validator->fails()) {
+                return $this->sendError('validation_error', $validator->errors(), 400);
+            }
+            if (!Auth::user()->hasPermission('answer_correct') && isset($input['user_id'])) {
+                return $this->sendError('no_permissions', [], 403);
+            }
+            if (isset($input['quiz'])) {
+                $query = Answer::where('quiz', 'quiz_'.$input['quiz']);
+            } elseif (isset($input['special_quiz'])) {
+                $query = Answer::where('quiz', 'special_quiz_'.$input['special_quiz']);
+            }
+            if (isset($input['user_id'])) {
+                $query = $query->where('user_id', $input['user_id']);
+            }
+            if (isset($input['submitted'])) {
+                $query = $query->where('submitted', $input['submitted']);
+            }
+            if (!Auth::user()->hasPermission('answer_correct')) {
+                $query = $query->where('user_id', Auth::user()->id);
+            }
+            $answers = $query->get();
+            return $this->sendResponse($answers, 200);
         }
-        if (!Auth::user()->hasPermission('answer_correct') && isset($input['user_id'])) {
-            return $this->sendError('no_permissions', [], 403);
-        }
-        if (isset($input['quiz'])) {
-            $query = Answer::where('quiz', 'quiz_'.$input['quiz']);
-        } elseif (isset($input['special_quiz'])) {
-            $query = Answer::where('quiz', 'special_quiz_'.$input['special_quiz']);
-        }
-        if (isset($input['user_id'])) {
-            $query = $query->where('user_id', $input['user_id']);
-        }
-        if (isset($input['submitted'])) {
-            $query = $query->where('submitted', $input['submitted']);
-        }
-        if (!Auth::user()->hasPermission('answer_correct')) {
-            $query = $query->where('user_id', Auth::user()->id);
-        }
-        $answers = $query->get();
-        return $this->sendResponse($answers, 200);
+        return $this->sendError('no_permissions', [], 403);
     }
 
     public function create(Request $request)
     {
-        $input = $request::all();
-        $validator = Validator::make($input, [
+        if (
+            Auth::user()->hasPermission('quiz_play') ||
+            Auth::user()->hasPermission('specialquiz_play')
+        ) {
+            $input = $request::all();
+            $validator = Validator::make($input, [
             'question_id' => 'exists:questions,id',
             'text' => 'string',
             'quiz' => 'required_without:special_quiz|exists:quizzes,id',
             'special_quiz' => 'required_without:quiz|exists:special_quizzes,id',
             'points' => 'integer|min:0|max:3',
         ]);
-        if ($validator->fails()) {
-            return $this->sendError('validation_error', $validator->errors(), 400);
-        }
-        $newAnswer = [
+            if ($validator->fails()) {
+                return $this->sendError('validation_error', $validator->errors(), 400);
+            }
+            $newAnswer = [
             'question_id' => intval($input['question_id']),
             'user_id' => Auth::id(),
             'text' => $input['text'],
@@ -67,13 +78,15 @@ class AnswerController extends BaseController
             'corrected' => 0,
             'submitted' => 0,
         ];
-        if (isset($input['quiz'])) {
-            $newAnswer['quiz'] = 'quiz_'.$input['quiz'];
-        } elseif (isset($input['special_quiz'])) {
-            $newAnswer['quiz'] = 'special_quiz_'.$input['special_quiz'];
+            if (isset($input['quiz'])) {
+                $newAnswer['quiz'] = 'quiz_'.$input['quiz'];
+            } elseif (isset($input['special_quiz'])) {
+                $newAnswer['quiz'] = 'special_quiz_'.$input['special_quiz'];
+            }
+            $answer = Answer::create($newAnswer);
+            return $this->sendResponse($input, 201);
         }
-        $answer = Answer::create($newAnswer);
-        return $this->sendResponse($input, 201);
+        return $this->sendError('no_permissions', [], 403);
     }
 
     public function update(Request $request)
