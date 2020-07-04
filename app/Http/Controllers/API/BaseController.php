9572d65ad58239a05e8ceac87620919cee38967a
André Ascensão <andremiguelaa@gmail.com>
2020-07-04 20:11:18 +0100

    remove not needed data from api responses

--- app/Http/Controllers/API/BaseController.php
+++ app/Http/Controllers/API/BaseController.php
@@ -8,20 +8,12 @@ class BaseController extends Controller
 {
     public function sendResponse($result = null, $code = 200)
     {
-        $response = [
-            'success' => true,
-        ];
-        if ($result || $result === []) {
-            $response['data'] = $result;
-        }
-
-        return response()->json($response, $code);
+        return response()->json($result, $code);
     }
 
     public function sendError($error, $errorMessages = [], $code = 404)
     {
         $response = [
-            'success' => false,
             'message' => $error,
         ];
 
