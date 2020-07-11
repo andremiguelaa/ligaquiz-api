427f097b2326db1732f46a8af2aafae5bdbed5e6
André Ascensão <andremiguelaa@gmail.com>
2020-07-11 18:47:41 +0100

    solo game result done

--- app/Http/Controllers/API/GameController.php
+++ app/Http/Controllers/API/GameController.php
@@ -49,7 +49,7 @@ class GameController extends BaseController
                         $query->where(function ($userQuery) use ($value) {
                             $userQuery->where('user_id_1', $value)->orWhere('user_id_2', $value);
                         });
-                    } else if($key === 'tier'){
+                    } elseif ($key === 'tier') {
                         $users = League::where('season', $input['season'])
                             ->where('tier', $input['tier'])->first()->user_ids;
                         $query->whereIn('user_id_1', $users)->orWhereIn('user_id_2', $users);
@@ -78,10 +78,109 @@ class GameController extends BaseController
                 $now = Carbon::now()->format('Y-m-d');
                 if ($game->quiz && $now > $game->quiz->date) {
                     $questionIds = $game->quiz->questions->pluck('question_id')->toArray();
-                    $game->answers = $answers
+                    $gameAnswers = $answers
                         ->whereIn('user_id', [$game->user_id_1, $game->user_id_2])
                         ->whereIn('question_id', $questionIds)
-                        ->groupBy(['user_id', 'question_id'])
+                        ->groupBy('user_id')->map(function ($user) {
+                            return $user->map(function ($answer) {
+                                unset($answer['user_id']);
+                                return $answer;
+                            });
+                        });
+                    
+                    
+
+                    $game->solo = $game->user_id_1 === $game->user_id_2;
+                    $game->user_id_1_correct_answers = 0;
+                    $game->user_id_1_game_points = 0;
+                    
+                    if (!isset($gameAnswers[$game->user_id_1])) {
+                        $game->user_id_1_game_points = 'F';
+                    } elseif ($gameAnswers[$game->user_id_1]->where('corrected', 0)->count()) {
+                        $game->user_id_1_game_points = 'P';
+                        $game->user_id_1_correct_answers = 'P';
+                    } else {
+                        $game->user_id_1_correct_answers = $gameAnswers[$game->user_id_1]
+                            ->where('correct', 1)->count();
+                        if ($game->solo) {
+                            $game->user_id_1_game_points = 1 + 0.5 * $game->user_id_1_correct_answers;
+                        }
+                    }
+
+                    if (!$game->solo) {
+                        $game->user_id_2_correct_answers = 0;
+                        $game->user_id_2_game_points = 0;
+                        if (!isset($gameAnswers[$game->user_id_2])) {
+                            $game->user_id_2_game_points = 'F';
+                        } elseif ($gameAnswers[$game->user_id_2]->where('corrected', 0)->count()) {
+                            $game->user_id_2_game_points = 'P';
+                            $game->user_id_2_correct_answers = 'P';
+                        } else {
+                            $game->user_id_2_correct_answers = $gameAnswers[$game->user_id_2]
+                                ->where('correct', 1)->count();
+                        }
+                    }
+
+                    
+
+                    /*
+                    if (
+                        $game->answers[$game->user_id_1]->where('corrected', 0) ||
+                        $game->answers[$game->user_id_2]->where('corrected', 0)
+                    ) {
+                        $game->user_id_1_game_points = 'P';
+                        if (!$game->solo) {
+                            $game->user_id_2_game_points = 'P';
+                        }
+                    }
+                    */
+
+                    // todo: calculate game result
+                    /*
+                    if (count($dayAnswers->where('corrected', 0))) {
+                        $players[0]['points'] = 'P';
+                        $players[1]['points'] = 'P';
+                        $players[0]['corrects'] = 'P';
+                        $players[1]['corrects'] = 'P';
+                        return $players;
+                    }
+                    $player1 = [];
+                    $player2 = [];
+                    foreach ($questionsIds as $questionId) {
+                        $player1[$questionId] = $dayAnswers->where('user_id', $this->user_1_id)->where('question_id', $questionId)->first();
+                        $player2[$questionId] = $dayAnswers->where('user_id', $this->user_2_id)->where('question_id', $questionId)->first();
+                        if ($player1[$questionId] && $player2[$questionId]) {
+                            $player1[$questionId]['pointsAttr'] = $player2[$questionId]->points;
+                            $player2[$questionId]['pointsAttr'] = $player1[$questionId]->points;
+                            $player1[$questionId]['pointsWon'] = $player1[$questionId]->correct * $player2[$questionId]->points;
+                            $player2[$questionId]['pointsWon'] = $player2[$questionId]->correct * $player1[$questionId]->points;
+                            $players[0]['points'] += $player1[$questionId]['pointsWon'];
+                            $players[1]['points'] += $player2[$questionId]['pointsWon'];
+                            $players[0]['corrects'] += $player1[$questionId]->correct;
+                            $players[1]['corrects'] += $player2[$questionId]->correct;
+                        } elseif (!$player1[$questionId] && !$player2[$questionId]) {
+                            $players[0]['points'] = 'F';
+                            $players[1]['points'] = 'F';
+                        } else {
+                            if ($player1[$questionId]) {
+                                $winner = 0;
+                                $loser = 1;
+                                $players[$winner]['corrects'] += $player1[$questionId]['correct'];
+                            } else {
+                                $winner = 1;
+                                $loser = 0;
+                                $players[$winner]['corrects'] += $player2[$questionId]['correct'];
+                            }
+                            $players[$loser]['corrects'] = 'F';
+                            $players[$loser]['points'] = 'F';
+                            $score = ['0' => 0, '1' => 2, '2' => 3, '3' => 5, '4' => 6, '5' => 8, '6' => 9, '7' => 10, '8' => 12];
+                            $players[$winner]['points'] = $score[$players[$winner]['corrects']];
+                        }
+                    }
+                    */
+
+                    /*
+                    $game->answers = $answers->groupBy(['user_id', 'question_id'])
                         ->map(function ($user) {
                             return $user->map(function ($question) {
                                 return $question->map(function ($answer) {
@@ -91,11 +190,7 @@ class GameController extends BaseController
                                 });
                             });
                         });
-                    $game->user_id_1_game_points = 0;
-                    $game->user_id_2_game_points = 0;
-                    $game->user_id_1_league_points = 0;
-                    $game->user_id_2_league_points = 0;
-                    // todo: calculate game result
+                    */
                 }
                 if ($game->quiz) {
                     $game->quiz->makeHidden('questions');
@@ -105,7 +200,7 @@ class GameController extends BaseController
 
             return $this->sendResponse([
                 'games' => $games,
-                'quizzes' => array_values($quizzes->toArray()),
+                // 'quizzes' => array_values($quizzes->toArray()),
             ], 200);
         }
         
