116948d383211c5672811262c66d1ff300574715
André Ascensão <andremiguelaa@gmail.com>
2020-08-11 23:07:00 +0100

    add past to season model

--- app/Season.php
+++ app/Season.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Season extends Model
 {
@@ -13,6 +14,17 @@ class Season extends Model
     protected $hidden = [
         'created_at', 'updated_at'
     ];
+
+    protected $appends = ['past'];
+
+    public function getPastAttribute()
+    {
+        $now = Carbon::now()->format('Y-m-d');
+        if ($now >= $this->rounds->first()->date) {
+            return true;
+        }
+        return false;
+    }
 
     public function leagues()
     {
