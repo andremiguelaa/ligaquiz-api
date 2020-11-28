7ad51ab7ada8293bdac1c9a895247331ee120717
André Ascensão <andremiguelaa@gmail.com>
2020-11-28 21:59:34 +0000

    create translations endpoint

--- app/Http/Controllers/QuestionsTranslationsController.php
+++ app/Http/Controllers/QuestionsTranslationsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
+use Request;
+use Validator;
 use App\Http\Controllers\BaseController as BaseController;
 use App\QuestionsTranslations;
 
@@ -12,6 +14,29 @@ class QuestionsTranslationsController extends BaseController
     {
         if (Auth::user()->hasPermission('translate')) {
             return $this->sendResponse(QuestionsTranslations::all(), 200);
+        }
+        return $this->sendError('no_permissions', [], 403);
+    }
+
+    public function create(Request $request)
+    {
+        if (Auth::user()->hasPermission('translate')) {
+            $input = $request::all();
+            $validator = Validator::make($input, [
+                'question_id' => [
+                    'required',
+                    'exists:questions,id',
+                    'unique:questions_translations,question_id'
+                ],
+                'content' => 'required|string',
+                'answer' => 'required|string'
+            ]);
+            if ($validator->fails()) {
+                return $this->sendError('validation_error', $validator->errors(), 400);
+            }
+            $input['user_id'] = Auth::user()->id;
+            $translation = QuestionsTranslations::create($input);
+            return $this->sendResponse($translation, 200);
         }
         return $this->sendError('no_permissions', [], 403);
     }
--- app/QuestionsTranslations.php
+++ app/QuestionsTranslations.php
@@ -6,4 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class QuestionsTranslations extends Model
 {
+    protected $fillable = [
+        'question_id',
+        'content',
+        'answer',
+        'user_id',
+    ];
+
+    protected $hidden = [
+        'created_at', 'updated_at'
+    ];
 }
