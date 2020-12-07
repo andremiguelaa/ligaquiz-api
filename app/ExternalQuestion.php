23ed4f3882943b416b91141a9e62d1d9e9dacf06
André Ascensão <andremiguelaa@gmail.com>
2020-12-07 20:16:31 +0000

    fix media attribute

--- app/ExternalQuestion.php
+++ app/ExternalQuestion.php
@@ -3,12 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Storage;
 
 class ExternalQuestion extends Model
 {
     protected $fillable = [
         'used',
     ];
+
+    public function getMediaAttribute($value)
+    {
+        return Storage::url('external/'.$value);
+    }
 
     protected $hidden = [
         'created_at', 'updated_at'
