ac22b4f617c9978e0e795510143411b877c2c8e1
André Ascensão <andremiguelaa@gmail.com>
2020-12-13 17:09:04 +0000

    fix mail footer link

--- resources/views/emails/partials/footer.blade.php
+++ resources/views/emails/partials/footer.blade.php
@@ -5,7 +5,7 @@
 <table>
     <tr>
         <td>
-            <a href="{{ env('APP_URL') }}">
+            <a href="{{ env('SPA_URL') }}">
                 <img src="{{ env('APP_URL') }}/logo.png" alt="Liga Quiz">
             </a>
         </td>
