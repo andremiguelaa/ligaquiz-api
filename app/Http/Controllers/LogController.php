fc8c6b51c4f52276abe75570af755930d2e9f856
André Ascensão <andremiguelaa@gmail.com>
2021-02-12 00:29:32 +0000

    version log

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -25,29 +25,54 @@ class LogController extends BaseController
             if ($validator->fails()) {
                 return $this->sendError('validation_error', $validator->errors(), 400);
             }
-            $queryLog = (new Log)->newQuery();
-            $queryAnswer = (new Answer)->newQuery();
-            if (isset($input['user_id'])) {
-                $queryLog = $queryLog->where('user_id', $input['user_id']);
-                $queryAnswer = $queryAnswer->where('user_id', $input['user_id']);
+            if (isset($input['versions'])) {
+                $queryLog = (new Log)->newQuery();
+                $queryLog = $queryLog->where('action', 'like', '%Page load%');
+                $lastWeek = Carbon::now()->addDay(-7);
+                $queryLog = $queryLog->where('created_at', '>', $lastWeek);
+                $data = $queryLog->orderBy('created_at', 'desc')->get();
+                $data =
+
+                $data = $data->groupBy('user_id')->map(function ($item) {
+                    $userVersion = 'N/A';
+                    foreach ($item as $key => $value) {
+                        preg_match('#\((.*?)\)#', $value->action, $version);
+                        if(isset($version[1])){
+                            $userVersion = $version[1];
+                            break;
+                        }
+                    }
+                    return [
+                        'user_id' => $item[0]->user_id,
+                        'version' => $userVersion,
+                    ];
+                })->toArray();
+                dd($data);
             }
-            if (isset($input['start_date'])) {
-                $queryLog = $queryLog->where('created_at', '>', $input['start_date']);
-                $queryAnswer = $queryAnswer->where('created_at', '>', $input['start_date']);
+            else {
+                $queryLog = (new Log)->newQuery();
+                $queryAnswer = (new Answer)->newQuery();
+                if (isset($input['user_id'])) {
+                    $queryLog = $queryLog->where('user_id', $input['user_id']);
+                    $queryAnswer = $queryAnswer->where('user_id', $input['user_id']);
+                }
+                if (isset($input['start_date'])) {
+                    $queryLog = $queryLog->where('created_at', '>', $input['start_date']);
+                    $queryAnswer = $queryAnswer->where('created_at', '>', $input['start_date']);
+                }
+                if (isset($input['end_date'])) {
+                    $endDate = Carbon::parse($input['end_date'])->addDay();
+                    $queryLog = $queryLog->where('created_at', '<', $endDate);
+                    $queryAnswer = $queryAnswer->where('created_at', '<', $endDate);
+                }
+                if (isset($input['search'])) {
+                    $queryLog = $queryLog->where('action', 'like', '%'.$input['search'].'%');
+                    $queryAnswer = $queryAnswer->where('text', 'like', '%'.$input['search'].'%');
+                }
+                $logs = $queryLog->get();
+                $answers = $queryAnswer->get();
+                $data = $logs->merge($answers)->sortBy('time')->values();
             }
-            if (isset($input['end_date'])) {
-                $endDate = Carbon::parse($input['end_date'])->addDay();
-                $queryLog = $queryLog->where('created_at', '<', $endDate);
-                $queryAnswer = $queryAnswer->where('created_at', '<', $endDate);
-            }
-            if (isset($input['search'])) {
-                $queryLog = $queryLog->where('action', 'like', '%'.$input['search'].'%');
-                $queryAnswer = $queryAnswer->where('text', 'like', '%'.$input['search'].'%');
-            }
-            $logs = $queryLog->get();
-            $answers = $queryAnswer->get();
-            $data = $logs->merge($answers)->sortBy('time')->values();
-            
             return $this->sendResponse($data, 200);
         }
         return $this->sendError('no_permissions', [], 403);
